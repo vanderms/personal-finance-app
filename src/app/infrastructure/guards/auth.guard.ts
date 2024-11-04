629ae2cff8253d7921c2d30b9ad27d000af0ff58
Vander Santos <vanderms.84@gmail.com>
2024-11-04 01:22:15 -0300

    fix auth guard

--- src/app/infrastructure/guards/auth.guard.ts
+++ src/app/infrastructure/guards/auth.guard.ts
@@ -18,7 +18,7 @@ export class AuthGuard implements CanActivate {
           this.router.navigate(['/']);
         }
       }),
-      map(() => false)
+      map((user) => Boolean(user))
     );
   }
 }
